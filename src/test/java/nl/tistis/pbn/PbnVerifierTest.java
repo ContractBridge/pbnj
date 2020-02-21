4a841debf285d891e2837b9927bbf2ece8e5ba0f
Christoph <ignatz@gmail.com>
2020-02-21 10:13:37 -0500

    added junit5 displayName annotations

--- src/test/java/nl/tistis/pbn/PbnVerifierTest.java
+++ src/test/java/nl/tistis/pbn/PbnVerifierTest.java
@@ -1,14 +1,18 @@
 package nl.tistis.pbn;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("PbnVerifier is the main method for the application")
 public class PbnVerifierTest {
+    @DisplayName("It has a test that needs to be deleted \uD83D\uDE31")
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 
+    @DisplayName("When I instantiate the class it should return an object")
     @Test void main() {
         PbnVerifier sut = new PbnVerifier(null, true);
         assertNotNull(sut, "PbnVerifier.main()");
